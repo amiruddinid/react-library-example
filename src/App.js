7d792d7d04bf81cb13f8f713ad7f9b83aea1b0b1
Amiruddin <amiruddinid@gmail.com>
2018-12-01 01:39:04 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import Select, { components } from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+// Ganti icons dropdown
+const DropdownIndicator = (props) => {
+  return components.DropdownIndicator && (
+    <components.DropdownIndicator {...props}>
+      <FontAwesomeIcon style={{color:'#fff'}} icon={props.selectProps.menuIsOpen ? faAngleUp  : faAngleDown}/>
+    </components.DropdownIndicator>
+  );
+};
+
+
+const customStyles = {
+  control: (provided) => ({
+    ...provided, // Style bawaan react-select
+    border: '1px solid blue',
+  }),
+  dropdownIndicator: (provided) => ({
+    ...provided, // Style bawaan react-select
+    background:'blue',
+    padding: '10px 10px',
+  }),
+  indicatorSeparator: () => ({
+    display: 'none'
+  })
+}
 
 class App extends Component {
+  state = {
+    selectedOption: null,
+  }
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  }
   render() {
+    const { selectedOption } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper" style={{ padding: '0 10px'}}>
+        <Select
+          components={{ DropdownIndicator }}
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+          styles={customStyles}
+        />
       </div>
     );
   }
